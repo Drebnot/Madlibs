189e98729cca2e24c303d52fa660cc9bff46526d
Jesse Drebin and Mike Chu <mike@>
2018-03-26 10:19:58 -0700

    add loop to refactor code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
-  $("#formOne").submit(funtion(event){
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-  })
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#"+ blank).val();
+      $("."+ blank).text(userInput);
+    });
+    $("#story").show();
+  });
 });
